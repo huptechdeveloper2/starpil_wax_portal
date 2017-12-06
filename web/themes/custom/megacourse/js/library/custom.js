8d7bdaa995b7410a9eb5fe86058fdddc43b9bf5c
nikunj-huptech <huptechdeveloper@gmail.com>
2017-12-06 09:47:41 +0530

    js issues in private message , checkbox css issues

--- web/themes/custom/megacourse/js/library/custom.js
+++ web/themes/custom/megacourse/js/library/custom.js
@@ -5,5 +5,9 @@ jQuery('.content-bar').on('click', 'li', function() {
       jQuery(this).addClass('current');
 });
 
+jQuery( ".private-message-inbox-thread-link" ).click(function() {
+  //alert( "Handler for .click() called." );
+  window.load();
+});
 
 })(jQuery);
